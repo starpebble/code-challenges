520ff05c51d77f2ceac8932d371d9a7e7f38837b
starpebble <github@spicykey.com>
2018-08-15 15:38:27 -0400

    teleporter solution

--- developers/README.md
+++ developers/README.md
@@ -1,13 +1,13 @@
 # Developer Code Challenges
 
-The purpose of this page is to a set of challenges that rotate fairly frequently that candidates can select from to deliver their coding assessment.  Below you will find answer to common questions along with the high level process to follow.  
+Here is my solution for the teleporter.  My solution requires NodeJS 8.11.2 and is written in javascript.
 
-* [Teleporter](teleporter.md)
-* [Wedding Seating](wedding-seating.md)
+* [Teleporter](teleporter.js)
 
 ## __Questions__
-### What to do?
-You will notice a series of Markdown files that are available, please select one of the challenges to work on and submit the results of your coding assessment in git repo that is accessible to the reviewer.  
+### How do I use it?
 
-### How do I submit my Challenge?
-To simplify the process we have opted to using GitHub as our primary source code repository for our challenges, please fork this repository and provide either a pull request or a link to a repository of your own that we can use to review the source code.
+Just install NodeJS.  Put the input into a file.  Then run the teleporter.js file with node, providing
+the filename on the command line.
+
+```node teleporter.js sample.txt```
---
+++ developers/teleporter.js
@@ -0,0 +1,131 @@
+'use strict';
+// starpebble for bytecubed
+const Graph=require('graph-data-structure');
+const fs=require('fs');
+
+const graph = Graph();
+
+if (process.argv.length < 3) {
+  console.warn("Please provide a file name as an argument");
+  process.exit();
+}
+
+function readGraph(lines) {
+  for (var line of lines) {
+    if (line.match('^.+ - .+$')) {
+      try {
+        var towns = line.split(' - ');
+        if (towns[0] !== towns[1]) {
+          graph.addEdge(towns[0], towns[1]);
+          graph.addEdge(towns[1], towns[0]);
+        } else {
+          console.warn('⚠️  Warning: Ignoring graph request where the from and to city are the same name: ' + line);
+        }
+      }
+      catch(e) {
+        console.log('My app had a problem reading this graph definition: ' + line);
+      }
+    }
+  }
+};
+
+function citiesnjumps(city,distance) {
+  var visited = new Set();
+  var tovisit = [];
+  var reachable = new Set();
+  tovisit.push(city);
+  for (var i = 0; i < distance; i++) {
+//            console.log(tovisit);
+    var newtovisit = new Set();
+    tovisit.forEach(v => {
+      var adjacent = graph.adjacent(v);
+//              console.log(adjacent);
+      visited.add(v);
+      adjacent.forEach(a => {
+        reachable.add(a);
+        if (!visited.has(a)) {
+          newtovisit.add(a);
+        }
+      });
+    });
+    tovisit = Array.from(newtovisit);
+  }
+  if (reachable.has(city)) {
+    reachable.delete(city);
+  }
+  return Array.from(reachable);
+}
+
+function canteleport(cityFrom, cityTo) {
+  var haspath = false;
+  try {
+    var path = graph.shortestPath(cityFrom,cityTo);
+    var haspath = true;
+  }
+  catch(e) {}
+  return haspath;
+}
+
+function cityhasloop(city) {
+  var hasloop = false;
+
+  var adjacent = graph.adjacent(city);
+  adjacent.forEach(a => {
+    graph.removeEdge(a,city);
+    var haspath = false;
+    try {
+      var path = graph.shortestPath(a,city);
+      hasloop = true;
+    }
+    catch(e) {}
+    graph.addEdge(a,city)
+  });
+
+  return hasloop;
+}
+
+function answerQuestions(lines) {
+  for (var line of lines) {
+    //example: cities from Summerton in 1 jumps
+    var citiesregex = /^cities from (.+) in (\d+) jumps$/;
+    var myArray = citiesregex.exec(line);
+    if (myArray && myArray.length === 3) {
+      var [l,city,distance] = myArray;
+      var cities = citiesnjumps(city,distance);
+      console.log(`${line}: ` + Array.from(cities).join(', ') );
+    }
+
+    //example: can I teleport from Springton to Atlantis
+    var canteleportregex = /^can I teleport from (.+) to (.+)$/;
+    var myArray = canteleportregex.exec(line);
+    if (myArray && myArray.length === 3) {
+      var [l,cityFrom,cityTo] = myArray;
+      var haspath = canteleport(cityFrom,cityTo);
+      console.log(`${line}: ${(haspath) ? 'yes' : 'no'}`)
+    }
+
+    //example: loop possible from Oaktown: yes
+    var looppossible = /^loop possible from (.+)$/;
+    var myArray = looppossible.exec(line);
+    if (myArray && myArray.length === 2) {
+      var city = myArray[1];
+      var hasloop = cityhasloop(city);
+      console.log(`${line}: ${(hasloop) ? 'yes' : 'no'}`);
+    }
+  }
+}
+
+const filename = process.argv[2];
+fs.readFile(filename, 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  var lines = data.split('\n');
+  lines = lines.filter(l => l.length > 0);
+  if (lines.length) {
+    readGraph(lines);
+    answerQuestions(lines);
+  } else {
+    console.warn('⚠️  Warning: There is no data in this file.');
+  }
+});
